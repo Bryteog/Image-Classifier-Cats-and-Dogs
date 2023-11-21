0fbacb176119cba1dfb2e806581a646fb4ae2f53
Bright Gyamfi <91860731+Bryteog@users.noreply.github.com>
2023-11-21 04:59:21 +0000

    Add files via upload

---
+++ model/Constant.py
@@ -0,0 +1,10 @@
+TRAIN_PATH = "../data/cats_and_dogs_filtered/Training"
+VALIDATION_PATH = "../data/cats_and_dogs_filtered/Validation"
+TEST_PATH = "../data/cats_and_dogs_filtered/Testing"
+
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 16
+NUM_WORKERS = 2
+DEVICE = 'cpu'
+SEED = 67
